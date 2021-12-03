f9d614edeef032707ff639a80adfc83249e293ee
JostinAlvarado552 <jealvarado2@utpl.edu.ec>
2021-12-03 16:13:24 -0500

    1

--- problema02/sol_python/Problema02.py
+++ problema02/sol_python/Problema02.py
@@ -6,13 +6,16 @@ precio= float(input("Ingrese el costo unitario del producto: "))
 cantidad= int(input("Ingrese la cantidad que desea de este producto: "))
 
 costoparcial = (precio*cantidad)
-print(costoparcial)
+
 descuento1 = (costoparcial*porcentaje1)/(100)
 descuento2 = (costoparcial*porcentaje2)/(100)
 descuento3 = (costoparcial*porcentaje3)/(100)
 descuento4 = (costoparcial*porcentaje4)/(100)
 
-print("Venta de Trajes\n" "Cantidad de productos:",cantidad,"\n" +"Costo de los productos:",precio,"\n"+"Subtotal:",costoparcial)
+print("Venta de Trajes\n" 
+      "Cantidad de productos:",cantidad,"\n"
+      +"Costo de los productos:",precio,"\n"
+        ""+"Subtotal:",costoparcial)
 if cantidad == 1:
     costofinal = costoparcial - descuento1
     print("Descuento:", descuento1)
--- problema08/sol_python/problema08.py
+++ problema08/sol_python/problema08.py
@@ -2,30 +2,29 @@ pies = 0
 yardas = 0
 pulgadas = 0
 metros = 0
-centimetros = 0
 contador = 0
 suma = 0
-cadena0 = ""
-cadena1 = ""
-cadena2 = ""
-cadena3 = " "
-cadena4 = " "
-cadenafinal = ""
+cadena0 = "Metros | Yardas | Pulgadas | Pies"
+cadena1 = 0
+cadena2 = 0
+cadena3 = 0
+cadena4 = 0
+
 metro = (int(input("Ingrese el valor en metros que desea convertir: ")))
-cadena0 = ("Metros | Yardas| Pulgada| Pies")
 print(cadena0)
-while contador<metro:
+while contador < metro:
     contador = contador + 1
     suma = contador
-    yardas = (suma * 1.094)
-    pulgadas = (suma * 39.37)
-    pies = (suma*3.2804)
-    cadena1 =suma
-    cadena2="%.2f" % yardas
-    cadena3 ="%.2f" % pulgadas
-    cadena4="%.2f" % pies
-    cadenafinal= cadena1,"",cadena2,cadena3,cadena4
-    print(cadenafinal)
+    yardas = suma * 1.094
+    pulgadas = suma * 39.37
+    pies = suma * 3.2804
+
+    cadena1 = suma
+    cadena2 = yardas
+    cadena3 = pulgadas
+    cadena4 = pies
+
+    print("  %.d      %.2f      %.2f     %.2f" % (cadena1, cadena2, cadena3, cadena4))
 
 
 
